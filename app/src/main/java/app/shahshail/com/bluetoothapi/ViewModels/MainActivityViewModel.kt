4d804a43af474aceb61eb2ef68cf5668bbd746ff
Shail Shah <shahshail9898@gmail.com>
2019-02-05 18:28:30 -0600

    Bluetooth api module added

---
+++ app/src/main/java/app/shahshail/com/bluetoothapi/ViewModels/MainActivityViewModel.kt
@@ -0,0 +1,9 @@
+package app.shahshail.com.bluetoothapi.ViewModels
+
+import android.arch.lifecycle.ViewModel
+
+class MainActivityViewModel  : ViewModel() {
+    init {
+
+    }
+}
--- app/src/main/java/app/shahshail/com/bluetoothapi/views/MainActivity.kt
+++ app/src/main/java/app/shahshail/com/bluetoothapi/views/MainActivity.kt
@@ -1,12 +1,14 @@
-package app.shahshail.com.bluetoothapi
+package app.shahshail.com.bluetoothapi.views
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import app.shahshail.com.bluetoothapi.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
